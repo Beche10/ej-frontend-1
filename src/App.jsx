fd17e3d3d4d86b9ba5ae664758c371dae4a474f8
Augusto <villevip10@gmail.com>
2024-06-22 19:03:21 -0300

    Avance 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <section> 
-      <main className="max-w-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-[1fr_350px] h-screen">
+      <main className="max-w-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-[1fr_350px] h-screen font-outfit">
      
      <Pokemons />
      <Aside />
