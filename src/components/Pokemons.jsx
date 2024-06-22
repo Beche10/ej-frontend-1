8595f7c2c8f0c20d3227badf54197301a18bf372
Augusto <villevip10@gmail.com>
2024-06-22 19:34:11 -0300

    GetAPI

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,8 +1,20 @@
+import axios from 'axios';
 import React from 'react'
 
 export const Pokemons = () => {
 
+    useEffect(() => {
+       
+        axios.get("http://localhost:8080/api/pokemones")
+        .then( () => console.log())
+        .catch( (error) => console.log(error))
+
+    }, [])
+    
+
+
+
     return (
-       <div>Pokemons</div>
+       <section></section>
   )
 };
