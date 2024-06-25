8aa38ffafabb73753e2ab49ebcefff49c1e5f1ab
Augusto <villevip10@gmail.com>
2024-06-25 10:57:23 -0300

    avances

--- src/components/PokemonPreview.jsx
+++ src/components/PokemonPreview.jsx
@@ -4,8 +4,7 @@ import { colorByType } from '../utils/constants'
 
 
 export const PokemonPreview = ({ pokemon }) => {  
-    
-  
+      
     return (
     <article className='text-center bg-white rounded-3xl relative font-semibold capitalize pb-4 shadow-slate-400/10 border-2 border-transparent hover:border-slate-200 cursor-pointer group grid gap-2'>
         <header className='h-28 mb-2 flex items-center justify-center overflow-hidden'>
--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -9,6 +9,17 @@ export const Pokemons = () => {
     const [allPokemones, setAllPokemones] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [pokemonName, setPokemonName] = useState('');
+
+    const pokemonByName = allPokemones.filter( (pokemon) => pokemon.name.toLowerCase().includes(pokemonName.toLowerCase()));   
+    console.log(pokemonByName);
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setPokemonName(e.target.pokemonName.value);
+    };
+
 
     
     useEffect(() => {
@@ -40,15 +51,21 @@ export const Pokemons = () => {
 
     return (
         <section className='p-4 py-5'>
-         <form>
+         <form onSubmit={handleSubmit}>
             <div className='bg-white p-4 flex rounded-2xl text-lg'>
-                <input className='outline-none flex-1' type="text" placeholder='Search your Pokemon..' /> 
+                <input 
+                className='outline-none flex-1' 
+                type="text" 
+                autoComplete='off'
+                placeholder='Search your Pokemon..' 
+                name='pokemonName'
+                /> 
                 <button className='bg-red-500 p-2 rounded-xl shadow-lg shadow-red-500/50  hover:bg-red-400 transition-colors'>
                 <IconSearch color='white' stroke={4}/>
                 </button>
             </div>
          </form>
-              <PokemonList pokemons={allPokemones}/>
+              <PokemonList pokemons={pokemonByName}/>
         </section>
     );
 };
