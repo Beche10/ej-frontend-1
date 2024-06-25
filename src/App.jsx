42ef7bff351b7dd0017a257a16a9f01b6b2d122f
Augusto <villevip10@gmail.com>
2024-06-25 01:29:08 -0300

    Avance estilos

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <section className="bg-[#d3d1d1] h-screen font-outfit overflow-y-auto"> 
+      <section className="bg-[#807e7e79] h-screen font-outfit overflow-y-auto"> 
         <main className="max-w-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-[1fr_350px]">
      
         <Pokemons />
--- src/components/PokemonPreview.jsx
+++ src/components/PokemonPreview.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
+import { colorByType } from '../utils/constants'
 
 
 
 export const PokemonPreview = ({ pokemon }) => {  
- 
+    
+  
     return (
-    <article className='text-center bg-white rounded-3xl relative font-semibold capitalize pb-2 shadow-slate-400/10 border-2 border-transparent hover:border-slate-200 cursor-pointer group'>
+    <article className='text-center bg-white rounded-3xl relative font-semibold capitalize pb-4 shadow-slate-400/10 border-2 border-transparent hover:border-slate-200 cursor-pointer group grid gap-2'>
         <header className='h-28 mb-2 flex items-center justify-center overflow-hidden'>
-        <img className='w-full h-full object-contain group-hover:scale-105'
+        <img className='w-full h-full object-contain group-hover:scale-110 transition-transform'
         src={pokemon.image} alt={pokemon.image}/>
         </header>
         <span className='text-sm text-slate-400'>N°{pokemon?.id}</span>
         <h4 className='text-lg'>{pokemon?.name}</h4>
         <ul className='flex gap-2 justify-center'>
         {pokemon?.type.map((type, index) => (
-          <li key={index}>{type}</li> 
+          <li className={`p-1 rounded-md px-2 text-white ${colorByType[type]}`} key={index}>{type}</li> 
          ))}
         </ul>
     </article>
