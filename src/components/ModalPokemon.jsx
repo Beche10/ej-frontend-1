0be98e170c39cf0c5c3d5261d0fb0733742fae34
Augusto <villevip10@gmail.com>
2024-07-08 20:23:03 -0300

    advance

--- src/components/ModalPokemon.jsx
+++ src/components/ModalPokemon.jsx
@@ -4,16 +4,17 @@ import { colorByType } from '../utils/constants'
 
 export const ModalPokemon = ({ showModal, onCloseModal, pokemon }) => {
   return (
-    <section className={`fixed top-0 left-0 right-0 h-screen bg-green-400 transition-all duration-500 
+    <section className={`fixed lg:hidden top-0 left-0 right-0 h-screen bg-green-400 transition-all duration-500 
       ${showModal ? 'visible opacity-100' : 'invisible opacity-0'}`}>
      
      <button onClick={onCloseModal} className='bg-white absolute top-4 right-4 p-1 rounded-lg hover:opacity-80 transition-opacity'>
         <IconX size={34} stroke={4}/>
      </button>
-     <article className={`bg-white h-[85%] absolute w-full rounded-tl-3xl rounded-tr-3xl text-center transition-all duration-500 px-4 grid gap-2 content-start ${showModal ? 'bottom-0' : '-bottom-full'}`}>
-       <header>
+     <article className={`bg-white h-[85%] absolute w-full rounded-tl-3xl rounded-tr-3xl text-center transition-all duration-500 ${showModal ? 'bottom-0' : '-bottom-full'}`}>
+       <header className='absolute left-1/2 -translate-x-1/2 -translate-y-[92%] scale-[180%]'>
         <img src={pokemon?.animatedImage} alt="" />
        </header>
+       <div className='overflow-y-auto px-4 pt-12 grid gap-2 content-start'>
        <span className='text-slate-400 text-sm font-semibold '> N°{pokemon?.id} </span> 
        <h2 className='font-bold text-lg capitalize'> {pokemon?.name} </h2>
        <ul className='flex gap-2 justify-center'>
@@ -37,7 +38,8 @@ export const ModalPokemon = ({ showModal, onCloseModal, pokemon }) => {
               </li>
             ))}
           </ul>
-        </section>
+         </section>
+        </div>
      </article>
     </section>
   )
