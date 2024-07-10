3f464d63e699a68ffeb3cf5760357f56f6756ee1
Augusto <villevip10@gmail.com>
2024-07-10 20:28:03 -0300

    Varios

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -9,12 +9,23 @@ export const Aside = ({ pokemon }) => {
   
   
   return (
-    
+
     <section className="hidden lg:block sticky top-0 h-screen">
               
-      <article className='absolute bottom-0 bg-white w-full h-[85%] rounded-tl-3xl rounded-tr-3xl text-center'> 
+
+      <article className='absolute bottom-0 bg-white w-full h-[85%] rounded-tl-3xl rounded-tr-3xl text-center hidden'> 
         
        <PokemonDetail pokemon={pokemon}/>
+        
+      </article>
+
+      <article className='absolute bottom-0 bg-white w-full h-[85%] rounded-tl-3xl rounded-tr-3xl text-center'> 
+       <header className='absolute left-1/2 -translate-x-1/2 '>
+        <img src="/no-pokemon-selected.png" alt="" />
+       </header> 
+       
+        <span>Select a Pokemon to display here.</span>
+
         
       </article>
     
